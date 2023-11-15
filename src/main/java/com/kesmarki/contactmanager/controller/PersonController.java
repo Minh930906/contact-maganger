aa410f9216311352730487e7f894df2832badb16
Minh930906 <minh930906@gmail.com>
2023-11-15 04:18:03 +0100

    added check for requestDtos and fix the getAll endpoint

--- src/main/java/com/kesmarki/contactmanager/controller/PersonController.java
+++ src/main/java/com/kesmarki/contactmanager/controller/PersonController.java
@@ -18,7 +18,7 @@ public class PersonController {
     private PersonService personService;
 
     @GetMapping
-    public List<Person> getAllPersons() {
+    public List<ResponsePersonDto> getAllPersons() {
         return personService.getAllPersons();
     }
 
--- src/main/java/com/kesmarki/contactmanager/service/PersonService.java
+++ src/main/java/com/kesmarki/contactmanager/service/PersonService.java
@@ -27,8 +27,20 @@ public class PersonService {
     @Autowired
     private AddressService addressService;
 
-    public List<Person> getAllPersons() {
-        return personRepository.findAll();
+    public List<ResponsePersonDto> getAllPersons() {
+        List<Person> personList = personRepository.findAll();
+        List<ResponsePersonDto> responsePersonDtos = new ArrayList<>();
+        for (Person person : personList){
+            List<Address> addresses = addressService.getAddressesByPersonId(person.getId());
+            List<AddressDTO> addressDTOList = AddressMapper.toDTOList(addresses);
+
+            ResponsePersonDto responsePersonDto = new ResponsePersonDto();
+            responsePersonDto.setId(person.getId());
+            responsePersonDto.setName(person.getName());
+            responsePersonDto.setAddresses(addressDTOList);
+            responsePersonDtos.add(responsePersonDto);
+        }
+        return responsePersonDtos;
     }
 
     public ResponsePersonDto getPersonById(Long id) {
@@ -36,7 +48,6 @@ public class PersonService {
 
         List<Address> addresses = addressService.getAddressesByPersonId(id);
         List<AddressDTO> addressDTOList = AddressMapper.toDTOList(addresses);
-
 
         ResponsePersonDto responsePersonDto = new ResponsePersonDto();
         responsePersonDto.setId(person.getId());
@@ -50,40 +61,44 @@ public class PersonService {
     public Person savePerson(RequestPersonDto requestPersonDto) {
         List<Address> addresses = new ArrayList<>();
 
-        for (AddressDTO addressDTO : requestPersonDto.getAddresses()) {
-            List<Contact> contacts = new ArrayList<>();
+        if (requestPersonDto.getAddresses() != null && !requestPersonDto.getAddresses().isEmpty()) {
+            for (AddressDTO addressDTO : requestPersonDto.getAddresses()) {
+                List<Contact> contacts = new ArrayList<>();
+                if (addressDTO.getContacts() != null && !addressDTO.getContacts().isEmpty()) {
 
-            for (ContactDTO contactDTO : addressDTO.getContacts()) {
-                Contact contact = new Contact();
-                contact.setType(contactDTO.getType());
-                contact.setContactValue(contactDTO.getContactValue());
-                contacts.add(contact);
+                    for (ContactDTO contactDTO : addressDTO.getContacts()) {
+                        Contact contact = new Contact();
+                        contact.setType(contactDTO.getType());
+                        contact.setContactValue(contactDTO.getContactValue());
+                        contacts.add(contact);
+                    }
+                }
+                Address address = new Address();
+                address.setType(addressDTO.getAddressType());
+                address.setCity(addressDTO.getCity());
+                address.setStreet(addressDTO.getStreet());
+                address.setContacts(contacts);
+                addresses.add(address);
             }
-
-            Address address = new Address();
-            address.setType(addressDTO.getAddressType());
-            address.setCity(addressDTO.getCity());
-            address.setStreet(addressDTO.getStreet());
-            address.setContacts(contacts);
-            addresses.add(address);
         }
 
         Person person = new Person();
         person.setName(requestPersonDto.getName());
-        person.setAddresses(addresses);  // Beállítjuk a címeket
 
-        // A személy mentése
+        if (requestPersonDto.getAddresses() != null && !requestPersonDto.getAddresses().isEmpty()) {
+            person.setAddresses(addresses);
+        }
+
         person = personRepository.save(person);
+        if (requestPersonDto.getAddresses() != null && !requestPersonDto.getAddresses().isEmpty()) {
+            for (Address address : addresses) {
+                address.setPerson(person);
+                address = addressService.saveAddress(address);
 
-        // Most, hogy a személynek már van id-je, beállítjuk az address és contact kapcsolatokat
-        for (Address address : addresses) {
-            address.setPerson(person);
-            address = addressService.saveAddress(address);
-
-            // Az id alapján beállítjuk a contact-oknak az address_id-t
-            for (Contact contact : address.getContacts()) {
-                contact.setAddress(address);
-                contactService.saveContact(contact);
+                for (Contact contact : address.getContacts()) {
+                    contact.setAddress(address);
+                    contactService.saveContact(contact);
+                }
             }
         }
         return person;
@@ -93,19 +108,23 @@ public class PersonService {
         Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
         person.setName(requestPersonDto.getName());
 
-        for (AddressDTO addressDTO : requestPersonDto.getAddresses()) {
-            Address address = addressService.getAddressById(addressDTO.getId());
-            address.setType(addressDTO.getAddressType());
-            address.setCity(addressDTO.getCity());
-            address.setStreet(addressDTO.getStreet());
-            for (ContactDTO contactDTO : addressDTO.getContacts()) {
-                Contact contact = contactService.getContactById(contactDTO.getId());
-                contact.setContactValue(contactDTO.getContactValue());
-                contact.setType(contactDTO.getType());
+        if (requestPersonDto.getAddresses() != null && !requestPersonDto.getAddresses().isEmpty()) {
+            for (AddressDTO addressDTO : requestPersonDto.getAddresses()) {
+                Address address = addressService.getAddressById(addressDTO.getId());
+                address.setType(addressDTO.getAddressType());
+                address.setCity(addressDTO.getCity());
+                address.setStreet(addressDTO.getStreet());
+                if (addressDTO.getContacts() != null && !addressDTO.getContacts().isEmpty()) {
+                    for (ContactDTO contactDTO : addressDTO.getContacts()) {
+                        Contact contact = contactService.getContactById(contactDTO.getId());
+                        contact.setContactValue(contactDTO.getContactValue());
+                        contact.setType(contactDTO.getType());
+                    }
+                }
+                addressService.saveAddress(address);
             }
-            addressService.saveAddress(address);
-
         }
+
         personRepository.save(person);
         return person;
     }
